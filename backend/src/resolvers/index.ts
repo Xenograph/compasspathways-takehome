fa406147105f71eb39b1db6ee269607064a7e020
Douglas Browne <douglas.browne123@gmail.com>
2024-03-20 02:11:28 -0400

    backend: delete unused Date resolver

--- backend/src/resolvers/index.ts
+++ backend/src/resolvers/index.ts
@@ -2,7 +2,6 @@ import { Resolvers } from "../generated/graphql.js";
 import Query from "./Query.js";
 import Customer from "./Customer.js";
 import Account from "./Account.js";
-import Date from "./Date.js";
 import Transaction from "./Transaction.js";
 
 const resolvers: Resolvers = {
