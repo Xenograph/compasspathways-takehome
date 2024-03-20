629066cf481587a7f65963723c81720e54e367a2
Douglas Browne <douglas.browne123@gmail.com>
2024-03-20 01:59:38 -0400

    frontend: use env variable for backend URL

--- frontend/src/lib/client.ts
+++ frontend/src/lib/client.ts
@@ -6,7 +6,7 @@ export const { getClient } = registerApolloClient(() => {
     cache: new InMemoryCache(),
     link: new HttpLink({
       // this needs to be an absolute url, as relative urls cannot be used in SSR
-      uri: "http://localhost:4000",
+      uri: process.env.NEXT_PUBLIC_BACKEND_URL || 'http://localhost:4000',
       // you can disable result caching here if you want to
       // (this does not work if you are rendering your page with `export const dynamic = "force-static"`)
       // fetchOptions: { cache: "no-store" },
